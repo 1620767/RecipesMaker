8f437c53c17876cddd5ab4b79c88c3079b630c2c
LuisPuse <lus@gmail.com>
2020-04-30 11:39:11 -0500

    v 0.001

--- src/main.kt
+++ src/main.kt
@@ -1,15 +1,17 @@
 fun main(args: Array<String>) {
 
-    inicio@ menu()
+    menu()
     var opcion:String?
 
-    inicio@ do {
+    do {
         opcion= readLine()
 
         if (opcion == "1"){
-            println("Empecemos")
+            makeRecipe()
         }else if (opcion == "2"){
-            println("Este es el listado de recetas")
+            viewRecipe()
+        }else if (opcion=="3"){
+            println("Bye")
         }else{
             println("Elije una opción valida")
         }
@@ -30,4 +32,25 @@ fun main(args: Array<String>) {
         """.trimIndent()
 
         println(encabezado);
+    }
+
+    fun makeRecipe(){
+        val r ="""
+            Hacer receta
+            Selecciona por categoría el ingrediente que buscas
+            1. Agua
+            2. Leche
+            3. Carne
+            4. Verduras
+            5. Frutas
+            6. Cereal
+            7. Huevos
+            8. Aceites
+        """.trimIndent()
+        println(r)
+    }
+
+    fun viewRecipe(){
+        val recetas = "Ver mis recetas"
+        println(recetas)
     }
